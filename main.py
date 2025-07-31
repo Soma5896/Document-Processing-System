c2b80ebc5145eafd9156af66911925735665de1f
Chaitanya Kumar Soma <157185314+Soma5896@users.noreply.github.com>
2025-07-31 12:42:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
 from app import api
 
+
 app = FastAPI()
 
 app.include_router(api.router)
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def root():
     return {"message": "Document Processor API"}
+
+
+
